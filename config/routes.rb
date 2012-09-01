6bf41c20a8ee9d4902cacea70db043882f9210ae
kubo0180 <gibson02122569@gmail.com>
2012-09-01 22:36:17 +0900

    devise setting is changed in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,19 @@ Cinemeeting::Application.routes.draw do
 
   root :to => 'service#index'
   
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  #devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  #devise_scope :user do
+  #  get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
+  #  get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  #end
+
+  evise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" } do
+    get 'sign_in', :to => 'users/sessions#new', :as => :new_user_session
+    get 'sign_out', :to => 'users/sessions#destroy', :as => :destroy_user_session
+  end
 
   devise_scope :user do
-    get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
-    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+    get '/users/auth/:provider' => 'users/omniauth_callbacks#passthru'
   end
 
   # The priority is based upon order of creation:
